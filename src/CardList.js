599fdb6b846a7726fcdb8d2611b561ba246ea8d6
Todd Whitelow <todd@whitelow.tech>
2023-03-03 16:11:11 -0700

    change to cardsArray name

--- src/CardList.js
+++ src/CardList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  const cardComponent = robots.map((user, i) => {
+  const cardsArray = robots.map((user, i) => {
     return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
   })
   return (
     <div>
-      {cardComponent}
+      {cardsArray}
     </div>
   );
 
