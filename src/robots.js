e73d787d1be99d68b74b4a48e9acad554d5c67ef
Todd Whitelow <todd@whitelow.tech>
2023-03-03 04:14:25 -0700

    create cards

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+    id: 0,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 1,
+    name: 'Ervin Howell',
+    username: 'Ervin',
+    email: 'Shanna@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Clemintine George',
+    username: 'Clem',
+    email: 'Nathan@uysia.net'
+  },
+  {
+    id: 3,
+    name: 'Johnny Brown',
+    username: 'Bhudda',
+    email: 'jaenk@freddy.biz'
+  }
+]
