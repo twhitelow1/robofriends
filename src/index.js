7c2fabdece12ef18e10ea7f3a4b54d4cc1f3b00a
Todd Whitelow <todd@whitelow.tech>
2023-03-09 11:31:00 -0700

    style scroll, structure app with folders, clean up app.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tachyons';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import App from './containers/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
