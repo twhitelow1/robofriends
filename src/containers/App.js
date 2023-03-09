5d13ad714c8611e133cc00b8406c4a11cc7ecbfb
Todd Whitelow <todd@whitelow.tech>
2023-03-09 16:56:59 -0700

    add error boundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundary';
 import './app.css'
 
 class App extends Component {
@@ -28,19 +29,19 @@ class App extends Component {
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase())
     })
-    if (!robots.length) {
-      return <h1>Loading</h1>
-    } else {
-      return (
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
-    }
   }
 
 }
